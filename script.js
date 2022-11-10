54129af29d45f143abd6672e1ce43a3d10f4b3ec
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2022-11-10 15:14:45 +0000

    delete and load cart

--- script.js
+++ script.js
@@ -1,50 +1,94 @@
 const boxContent = document.getElementById("card-box");
-const cartList = document.getElementById("cart-list");
+const shoppingCart = document.getElementById("shopping-cart");
 
-// const getData = () => {
-//   fetch("https://striveschool-api.herokuapp.com/books")
-//     .then((response) => response.json())
-//     .then((listOfBooks) => {
-//       listOfBooks.forEach((book) => {
-//         boxContent.innerHTML += `
-// <div class="col mb-4">
-//   <div class="card mb-3 ">
-//     <img src="${book.img}" class="card-img-top imagem" alt="..." />
-//     <div class="card-body">
-//       <h5 class="card-title truncate">${book.title}</h5>
-//       <p class="card-text">Price</p>
-//       <a href="#" class="btn btn-primary">Cart</a>
-//       <a href="#" class="btn btn-primary">Skip</a>
-//     </div>
-//   </div>
-// </div>`;
-//       });
-//     })
-//     .catch(() => (err) => console.error(err));
-// };
+let outerBooks = [];
+let shoppingCartList = [];
+let filteredBooks = [];
 
 async function getData() {
-  const response = await fetch("https://striveschool-api.herokuapp.com/books");
-  const listOfBooks = await response.json();
-  return listOfBooks;
+  try {
+    const response = await fetch(
+      "https://striveschool-api.herokuapp.com/books"
+    );
+    const books = await response.json();
+    displayBooks(books);
+    outerBooks = books;
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-async function displayBooks() {
-  const listOfBooks = await getData();
-  listOfBooks.forEach((book) => {
+async function displayBooks(books) {
+  books.forEach((book) => {
     boxContent.innerHTML += `
-    <div class="col mb-4">
+    <div class="col mb-4 card-box">
      <div class="card mb-3 ">
-       <img src="${book.img}" class="card-img-top imagem" alt="..." />
+       <img src="${book.img}" class="card-img-top imagem" alt="${book.title}" />
        <div class="card-body">
          <h5 class="card-title truncate">${book.title}</h5>
-         <a href="#" class="btn btn-primary cart">Cart</a>
-         <a href="#" class="btn btn-primary">Skip</a>
+         <button class="btn btn-primary" onclick="addToCart('${String(
+           book.asin
+         )}', this)">$${book.price}</button>
+          <button class="btn btn-warning" onclick="this.closest('.card-box').remove()">
+         Skip me
+         </button>
        </div>
      </div>
     </div>`;
   });
 }
 
+function addToCart(asin, element) {
+  console.log(asin);
+  const book = outerBooks.find((book) => book.asin == asin);
+  shoppingCartList.push(book);
+  console.log(shoppingCartList);
+  refreshShoppingCart();
+  element.closest(".card").classList.add("selected");
+}
+
+function refreshShoppingCart() {
+  shoppingCart.innerHTML = "";
+
+  shoppingCartList.forEach((book) => {
+    shoppingCart.innerHTML += `
+  <div class="shopping-item">
+    <div>
+      ${book.title}
+    </div>
+    <div>
+      ${book.price}
+    </div>
+    <div>
+      <button class="btn btn-danger" onclick="deleteItem('${String(
+        book.asin
+      )}')">Delete </button>
+    </div>
+  </div>
+`;
+  });
+}
+
+function search(query) {
+  if (query.length > 3) {
+    filteredBooks = outerBooks.filter((book) =>
+      book.title.toLowerCase().includes(query.toLowerCase())
+    );
+
+    displayBooks(filteredBooks);
+  } else {
+    displayBooks(outerBooks);
+  }
+}
+
+function deleteItem(asin) {
+  const index = shoppingCartList.findIndex((book) => book.asin === asin);
+
+  if (index !== -1) {
+    shoppingCartList.splice(index, 1);
+  }
+
+  refreshShoppingCart();
+}
+
 getData();
-displayBooks();
